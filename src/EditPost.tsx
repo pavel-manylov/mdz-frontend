062363b19bcbe001a340db329d60377feca6918b
Pavel Manylov <pavel@manylov.me>
2021-08-07 00:43:38 +0500

    MTP-42 Дублирование нового компонента при вторичном сохранении отредактированной публикации: EditPost refactoring

--- src/EditPost.tsx
+++ src/EditPost.tsx
@@ -1,18 +1,55 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {Component, NewComponent, Post} from "./api";
-import {OldPostForm} from "./OldPostForm";
 import Config from "./Config";
 import {Alert, Button, Spinner} from "react-bootstrap";
+import {PostForm} from "./PostForm";
+
+async function updatePost(post: Post, components: (Component | NewComponent)[]): Promise<[Post, Component[]]> {
+    try {
+        const postResponse = await Config.postApi.updatePost(post.id as number, post);
+        const componentsIndexResponse = await Config.componentApi.indexComponents(post.id as number);
+
+        const updatedPost = postResponse.data;
+
+        const initialComponentsIds = componentsIndexResponse.data.map(c => c.id);
+        const currentComponentsIds = components.map(c => "id" in c ? c.id as number : undefined).filter(c => c !== undefined);
+        const componentsIdsToDelete = initialComponentsIds.filter(id => !currentComponentsIds.includes(id));
+
+        for (const componentIdToDelete of componentsIdsToDelete) {
+            await Config.componentApi.deleteComponent(post.id as number, componentIdToDelete as number);
+        }
+
+        let updatedComponents: Component[] = [];
+
+        for (const component of components) {
+            let componentResponse;
+            if ("id" in component) {
+                componentResponse = await Config.componentApi.updateComponent(postResponse.data.id as number, (component as Component).id as number, component);
+            } else {
+                componentResponse = await Config.componentApi.createComponent(postResponse.data.id as number, component as NewComponent);
+            }
+            updatedComponents.push(componentResponse.data);
+        }
+
+        return [updatedPost, updatedComponents];
+    } catch (e) {
+        if (e.response && e.response.status === 422) {
+            throw(new Error("Форма заполнена некорректно: " + e.response.data.errors.join(", ")));
+        } else {
+            throw(new Error("Произошла ошибка во время создания публикации"));
+        }
+    }
+}
 
 export function EditPost() {
     let {id}: any = useParams();
+    const [post, setPost] = useState<Post | undefined>();
+    const [components, setComponents] = useState<(Component | NewComponent)[] | undefined>();
     const [saveSuccess, setSaveSuccess] = useState<boolean>(false);
     const [saveError, setSaveError] = useState<string>("");
-    const [post, setPost] = useState<Post | undefined>();
     const [loading, setLoading] = useState<boolean>(true);
     const [loadingError, setLoadingError] = useState<string | undefined>();
-    const [components, setComponents] = useState<(Component | NewComponent)[] | undefined>();
 
     async function load() {
         setLoading(true);
@@ -31,7 +68,29 @@ export function EditPost() {
 
     useEffect(() => {
         load();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    async function onSave() {
+        if (post === undefined || components === undefined) return;
+
+        setSaveSuccess(false);
+        setSaveError("");
+
+        if (post.name === "" || post.seo_url === "") {
+            setSaveError("Необходимо заполнить все поля формы");
+            return;
+        }
+
+        try {
+            const [updatedPost, updatedComponents] = await updatePost(post, components);
+            setPost(updatedPost);
+            setComponents(updatedComponents);
+            setSaveSuccess(true);
+        } catch (e) {
+            setSaveError(e.message);
+        }
+    }
 
     return (
         <div>
@@ -39,7 +98,7 @@ export function EditPost() {
 
             <Alert show={!!loadingError} variant="danger">
                 <p>{loadingError}</p>
-                <Button onClick={e => load()}>Повторить попытку</Button>
+                <Button onClick={() => load()}>Повторить попытку</Button>
             </Alert>
 
             {loading ? <Spinner animation="border" variant="primary"/> : ''}
@@ -53,16 +112,13 @@ export function EditPost() {
                     <Alert show={saveError !== ""} variant="danger">
                         <p>Не удалось сохранить публикацию. {saveError}</p>
                     </Alert>
-                    <OldPostForm post={post} saveText="Сохранить" components={components}
-                                 onSaveSuccess={() => {
-                                  setSaveError("");
-                                  setSaveSuccess(true);
-                              }}
-                                 onSaveError={(errorMessage) => {
-                                  setSaveError(errorMessage);
-                                  setSaveSuccess(false);
-                              }}
-                    />
+
+                    <PostForm post={post}
+                              components={components}
+                              saveText="Сохранить"
+                              onSave={onSave}
+                              onChangePost={setPost}
+                              onChangeComponents={setComponents}/>
                 </div>
             }
         </div>
